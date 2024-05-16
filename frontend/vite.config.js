a5a345091cd23881d1d35fa438bdeda0513c27b3
Raghav <121198446+raghv-m@users.noreply.github.com>
2024-05-16 03:39:37 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://chat-app-c4nn.onrender.com",
+				target: "https://chat-app-1-a73v.onrender.com/",
 			},
 		},
 	},
